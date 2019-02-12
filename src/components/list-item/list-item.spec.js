b52ef9ab968e47ee3ca4a8c8e0e3d7f5f390aee2
Philipp Runge <p.runge@progani.com>
2019-02-12 15:54:14 +0100

    jest installed and bundled with eslint, removed dummy tests, prepared first test for c-list-item

--- src/components/list-item/list-item.spec.js
+++ src/components/list-item/list-item.spec.js
@@ -1,29 +1,12 @@
-import Vue from 'vue';
-import ListItemComponent from './index.vue';
+import { shallowMount } from '@vue/test-utils'
+import ListItem from './list-item';
 
-// Here are some Jasmine 2.0 tests, though you can
-// use any test runner / assertion library combo you prefer
-describe('ListItemComponent', () => {
-  // Inspect the raw component options
-  it('has a created hook', () => {
-    // expect(typeof ListItemComponent.created).toBe('function');
-  })
-  // Evaluate the results of functions in
-  // the raw component options
-  it('sets the correct default data', () => {
-    // expect(typeof ListItemComponent.data).toBe('function')
-    // const defaultData = ListItemComponent.data();
-    // expect(defaultData.message).toBe('hello!');
-  })
-  // Inspect the component instance on mount
-  it('correctly sets the message when created', () => {
-    // const vm = new Vue(ListItemComponent).$mount();
-    // expect(vm.message).toBe('bye!');
-  })
-  // Mount an instance and inspect the render output
-  it('renders the correct message', () => {
-    // const Ctor = Vue.extend(ListItemComponent);
-    // const vm = new Ctor().$mount();
-    // expect(vm.$el.textContent).toBe('bye!');
-  })
+describe('ListItem', () => {
+  const listItem = shallowMount(ListItem, {
+    propsData: {
+      title: 'I am a title'
+    }
+  });
+
+  expect(listItem.vm.title).toMatch('I am a title');
 })
