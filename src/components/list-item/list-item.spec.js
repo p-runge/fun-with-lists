fe34b7362f8b31194e4d8a2cb3d718cf59db46ed
Philipp Runge <p.runge@progani.com>
2019-02-12 17:20:22 +0100

    implemented jest

--- src/components/list-item/list-item.spec.js
+++ src/components/list-item/list-item.spec.js
@@ -1,12 +1,14 @@
 import { shallowMount } from '@vue/test-utils'
-import ListItem from './list-item';
+import ListItem from '@/components/list-item'
 
 describe('ListItem', () => {
-  const listItem = shallowMount(ListItem, {
-    propsData: {
-      title: 'I am a title'
-    }
-  });
+  it('properties get passed', () => {
+    const listItem = shallowMount(ListItem, {
+      propsData: {
+        title: 'I am a title'
+      }
+    });
 
-  expect(listItem.vm.title).toMatch('I am a title');
+    expect(listItem.vm.title).toMatch('I am a title')
+  });
 })
